f0964da5d52ed663ddd8f52b5ec3fc21dee743f6
Merlin <merlin.anbazhagan@cognizant.com>
2025-08-12 18:57:42 +0530

    test changes

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -89,7 +89,7 @@ export async function POST(req: Request) {
 
     const companyRes = await fetch(`https://api-b2b.bigcommerce.com/api/v3/io/companies`, {
         headers: {
-          'X-Auth-Token': process.env.BC_API_TOKEN as string,
+          'authToken': `Bearer ${process.env.BC_B2B_AUTH_TOKEN}`,
           'Content-Type': 'application/json',
           Accept: 'application/json',
         },

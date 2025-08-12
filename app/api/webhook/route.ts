1582cf3e982f8efc49819c7af88fe23aff7310b1
Merlin <merlin.anbazhagan@cognizant.com>
2025-08-12 17:43:42 +0530

    test commit with company details

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -47,6 +47,7 @@ export async function POST(req: Request) {
     console.log('Webhook Payload:', body);
  
     const orderId = body.data?.id;
+    const companyName =body.data?.billing_address?.company;
     if (!orderId) {
       return NextResponse.json({ success: false, error: 'Order ID not found in payload' });
     }
@@ -76,10 +77,22 @@ export async function POST(req: Request) {
     console.log('Products:', products);
  
    
+    const companyRes = await fetch(`https://api-b2b.bigcommerce.com/api/v3/io/companies?q=${companyName}`, {
+        headers: {
+          Authorization: `Bearer ${process.env.BC_B2B_AUTH_TOKEN}`,
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      });
+    if (!companyRes.ok) throw new Error('Failed to fetch Customer Data');
+    const companies: Customer = await companyRes.json();
+    console.log('Customer:', companies);
+
     return NextResponse.json({
       success: true,
       order,
-      products
+      products,
+      companies
      // customer,
       //companyId,
       //e8CompanyId,

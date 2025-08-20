d4c162aabc9a77bd552acd672f011a2b7cc51fdf
Merlin <merlin.anbazhagan@cognizant.com>
2025-08-20 12:32:17 +0530

    test changes

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -138,13 +138,13 @@ const productDetails = products.map(product => ({
 
 // Update the Order status if its Junior Buyer
 
-if(userCompany.companyRoleId ===22405){
-  console.log("In Update Order Method")
-  const status =1;
-  const customer_message="Order submitted by Junior Buyer, awaiting Senior Buyer approval."
-  const orderStatusUpdate = await updateOrderStatus(orderId,status,customer_message);
-  console.log("Order Status Update",orderStatusUpdate);
-}
+// if(userCompany.companyRoleId ===22405){
+//   console.log("In Update Order Method")
+//   const status =1;
+//   const customer_message="Order submitted by Junior Buyer, awaiting Senior Buyer approval."
+//   const orderStatusUpdate = await updateOrderStatus(orderId,status,customer_message);
+//   console.log("Order Status Update",orderStatusUpdate);
+// }
 
 
 const companyDetails = await fetchCompanyDetailsByName(companyName);
